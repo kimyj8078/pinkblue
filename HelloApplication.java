d056ae6aee54a30c2888f0f47c4c4161697b2b84
kimyj8078 <applecap88@nate.com>
2021-05-30 19:15:02 +0900

    2021.05.30_JavaTest

---
+++ HelloApplication.java
@@ -0,0 +1,6 @@
+public class HelloApplication {
+   public static void main(String args[]) { // main method
+      /* Display the string */
+     System.out.println("Hello, Java Application!");
+    }
+  }
