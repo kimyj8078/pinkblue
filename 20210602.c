31158f82c18b114333f3e99888d7219886565066
kimyj8078 <applecap88@nate.com>
2021-06-02 23:53:16 +0900

    2021.06.02_C_Test

---
+++ 20210602.c
@@ -0,0 +1,12 @@
+/* 변수의 번지를 출력하는 프로그램 */
+#include <stdio.h> 
+int main(){
+	int i=127;
+	int j;
+	
+	printf("%d, %x \n", i, i);			//변수 i의 값을 10진수와 16진수로 출력
+	printf("%x \n", &i);				//&연산자: 주소연산자
+	printf("%#x \n", &i);
+	printf("%x \n", &j);
+	printf("%#x \n", &j); 
+}
