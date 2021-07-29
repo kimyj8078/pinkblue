9111e11552cb13223042c761ca6f68bdc1572751
kimyj8078 <applecap88@nate.com>
2021-07-29 23:59:26 +0900

    2021.07.29_JavaTest(12)

---
+++ JavaPrimaryType.java
@@ -0,0 +1,14 @@
+//[복습]기본 타입 선언하기
+//(2021.07.29) 
+//출처: Java200제
+
+public class JavaPrimaryType {
+	public static void main(String[] args) {
+		boolean isL=false;
+		char suit='H';
+		int year=2016;
+		double latitude=37.52127220511242;
+		double longitude=127.0074462890625;
+		System.out.println(latitude+"\t"+longitude);
+	}
+}
